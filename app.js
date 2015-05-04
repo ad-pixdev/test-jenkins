56c08172de23079080db8ef8e1059ab9ae0ee966
ad-pixdev <adrien@pixdev.fr>
2015-05-04 16:31:35 +0200

    rebuild with fail issue for testing mail

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins !');
+  res.send('hello jenkins');
 });
 
 app.get('/mail', function (req, res) {
